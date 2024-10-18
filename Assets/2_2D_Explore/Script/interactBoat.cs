3a785c2c8cbf1e79c551102a49e4218932ed2072
Calvin Julianza Rindy Hartanto <calvinjulianza@gt.student.pens.ac.id>
2024-10-18 09:19:15 +0700

    Update method change scene

--- Assets/2_2D_Explore/Script/interactBoat.cs
+++ Assets/2_2D_Explore/Script/interactBoat.cs
@@ -6,7 +6,6 @@ using UnityEngine.SceneManagement; // Tambahkan referensi ke SceneManager
 namespace MG_2DExplore {
     public class boatInteract : MonoBehaviour
     {
-        public string sceneName; // Nama scene yang akan dipindahkan
 
         void Update()
         {
@@ -14,7 +13,14 @@ namespace MG_2DExplore {
             if (Input.GetKeyDown(KeyCode.E))
             {
                 // Pindah ke scene yang ditentukan
-                SceneManager.LoadScene(sceneName);
+            if (SceneLoader.Instance != null)
+                {
+                    SceneLoader.Instance._MGReeling(); // Ganti dengan metode yang sesuai
+                }
+            else
+                {
+                    Debug.LogError("SceneLoader tidak ditemukan!");
+                }
             }
         }
     }
